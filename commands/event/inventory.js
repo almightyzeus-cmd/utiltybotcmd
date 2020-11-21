bf1f6e159932b860cc8ce04e29238e02e1f6bf7b
Glitch (hello-express) <none>
2020-11-21 14:56:44 +0000

    🍡😂 Checkpoint ./commands/main/invite.js:24440941/2434 ./commands/main/ping.js:24440941/36 ./commands/event/inventory.js:24440941/1453

--- commands/event/inventory.js
+++ commands/event/inventory.js
@@ -11,5 +11,20 @@ authorPermission: [],
 aliases: [],
 description: "See your inventory.",
 run: async (client, message, args) => {
+  let s = db.startsWith(`nikes_${message.guild.id}`, { sort: '.data'})
+    let content = "";
+
+    for (let i = 0; i < nike.length; i++) {
+        let user = client.users.get(nike[i].ID.split('_')[2]).username
+
+        content += `${i+1}. ${user} ~ ${nike[i].data}\n`
+    }
+
+    const embed = new Discord.RichEmbed()
+    .setDescription(`**${message.guild.name}'s Fresh Nikes Leaderboard**\n\n${content}`)
+    .setColor("#FFFFFF")
+
+    message.channel.send(embed)
 }
-};
+}
+;
--- commands/main/invite.js
+++ commands/main/invite.js
@@ -2,20 +2,18 @@ const discord = require("discord.js");
 const db = require("quick.db");
 
 module.exports = {
-  name: "inventory",
-  usage: "we!inventory",
+  name: "invite",
+  usage: "we!invite",
   ownerOnly: true,
   cooldown: 5000,
+  category: "info",
   botPermission: [],
   authorPermission: [],
   aliases: [],
-  description: "See your inventory.",
+  description: "sends a bot invite.",
   run: async (client, message, args) => {
     message.delete();
-    let invite =
-      "https://discord.com/api/oauth2/authorize?client_id=" +
-      this.client.user.id +
-      "&permissions=8&scope=bot";
+    let invite = `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`;
     message.channel.send("The invite was sent in your DMs.");
     return message.author.send(invite);
   }
