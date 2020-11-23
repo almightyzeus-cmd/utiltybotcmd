76a1f2c3e91edb1b686d28b49029e254785894f3
Glitch (hello-express) <none>
2020-11-23 17:41:32 +0000

    🙋📡 Checkpoint ./commands/event/shop.js:23557778/405 ./config.json:23557778/68 ./commands/event/bal.js:23557778/4

--- commands/event/shop.js
+++ commands/event/shop.js
@@ -1,17 +1,20 @@
 const discord = require("discord.js");
 
 module.exports = {
-name: "shop"
-usage: "Noth",
+name: "shop",
+usage: "we!shop",
 ownerOnly: false, 
 cooldown: 5000,
 botPermission: [],
 authorPermission: [],
 aliases: [],
-description: "Nothing",
+description: "View the shop.",
 run: async (client, message, args) => {
   const shop = new discord.MessageEmbed()
   .setTitle('Shop:')
+  .setDescription('ID=SA3. 30 Points: Special advertiser role for 30 days!\n ID=PZA 100 Points: Premium Zone Access\n ID=OAC 200 Points: Get own ad channel in ad channels category + spotlight post with @others ping!')
+  .setFooter('You can buy those with Event Points.')
+  message.channel.send(shop)
   
 }
-}
+};
