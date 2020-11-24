807ac436ea6624e142115c6615ff28db3e4cd0e9
Glitch (hello-express) <none>
2020-11-24 14:32:08 +0000

    🚥👧 Checkpoint ./commands/event/use.js:23557778/293

---
+++ commands/event/use.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+name: "use",
+usage: "we!use <itemID>",
+ownerOnly: false, 
+cooldown: 5000,
+botPermission: [],
+authorPermission: [],
+aliases: [],
+description: "Use an item in your inventory!",
+run: async (client, message, args) => {
+  
+}
+}
