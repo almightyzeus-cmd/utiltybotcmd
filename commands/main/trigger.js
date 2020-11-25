5ce691098a81cfc0b48d18a9a2e4d4c9d77a9f88
Glitch (wa-events) <none>
2020-11-25 07:19:49 +0000

    👛💗 Checkpoint ./commands/main/trigger.js:24440941/232 ./package.json:24440941/3264

--- commands/main/trigger.js
+++ commands/main/trigger.js
@@ -10,10 +10,10 @@ module.exports = {
 
         let avatar = message.author.displayAvatarURL({dynamic: false, format: "png"});
 
-        let image = await canva.trigger(avatar);
+        let image = canva.jail(avatar, true)
 
-        let triggered = new Discord.MessageAttachment(image, "triggered.gif")
+        let triggered = new Discord.MessageAttachment(image, "triggered.png")
 
-        message.channel.send(triggered);
+        message.channel.send(image);
     }
 }
