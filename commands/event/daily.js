e4847b9032a02ade9b584e45ed09ea90808a9853
Glitch (wa-events) <none>
2020-11-25 14:23:33 +0000

    💖😋 Checkpoint ./commands/event/daily.js:23557778/28

--- commands/event/daily.js
+++ commands/event/daily.js
@@ -7,8 +7,8 @@ module.exports = {
 
     async run (client, message, args) {
         let user = message.author;
-        let timeout = 86400000;
-        let amount = 500;
+        let timeout = 604800000;
+        let amount = 50;
 
         let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
 
