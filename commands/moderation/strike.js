2bb254af706ef66c955c18ca8aab010549421d08
Glitch (carnation-glib-trout) <none>
2020-12-11 18:19:34 +0000

    👛💎 Checkpoint ./commands/moderation/strike.js:23557778/669

--- commands/moderation/strike.js
+++ commands/moderation/strike.js
@@ -1,4 +1,5 @@
 const discord = require('discord.js')
+const db = require('quick.db')
 module.exports = {
   name: "strike",
   usage: "wa!strike <user> <reason>",
@@ -12,8 +13,15 @@ module.exports = {
   description: "Nothing",
   run: async (client, message, args) => {
     const user = message.mentions.members.first()
+    if(!message.mentions.members.first()) {
+      message.channel.send("Please mention a user to strike.")
+    }
+    db.add(`strikes_${user}`, 1)
+    const strikesamount = db.get(`strikes_${user}`)
     const strikeembed = new discord.MessageEmbed()
    .setTitle("Striked!")
-  .setDescription(`Successfully striked`)
+   .setDescription(`Successfully striked <@${user.id}>, they are now at ${strikesamount} strikes!`)
+   .setTimestamp()
+    client.channels.get('771837678823931937').send(strikeembed);
   }
 };
