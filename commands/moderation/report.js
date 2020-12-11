fc1363bd4e9afdb779f9dadcfa47816fc0b8074f
Glitch (carnation-glib-trout) <none>
2020-12-11 20:02:16 +0000

    💡🌈 Checkpoint ./commands/moderation/report.js:23557778/584

--- commands/moderation/report.js
+++ commands/moderation/report.js
@@ -9,12 +9,11 @@ authorPermission: [],
 aliases: [],
 description: "Nothing",
 run: async (client, message, args) => {
-  const suspect = message.mentions.members.first()
   const reportembed = new discord.MessageEmbed()
   .setAuthor('Report')
   .setTitle('From Welcome Advertisements | Free Unlimited Advertising')
   .addField('Reported By', `${message.author.tag}`, true)
-  .addField('Suspect', `${suspect.tag}`, true)
+  .addField('Suspect', `<@${message.mentions.members.first().id}>`, true)
   .setTimestamp()
   const channel = client.channels.cache.get('778175703358373949');
     channel.send(reportembed);
