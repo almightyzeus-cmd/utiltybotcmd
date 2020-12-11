c7a96d3f30bf5a5cbe53b140232094b474e66399
Glitch (carnation-glib-trout) <none>
2020-12-11 18:30:58 +0000

    💿🐿 Checkpoint ./commands/moderation/strike.js:23557778/105 ./commands/moderation/strikeremove.js:23557778/1026 ./commands/utility/timer.js:23557778/627

--- commands/moderation/strikeremove.js
+++ commands/moderation/strikeremove.js
@@ -1,8 +1,8 @@
 const discord = require('discord.js')
 const db = require('quick.db')
 module.exports = {
-  name: "strike",
-  usage: "wa!strike <user>",
+  name: "removestrike",
+  usage: "wa!unstrike <user> <amount>",
   category: "moderation",
   ownerOnly: false,
   strikeOnly: true,
@@ -14,14 +14,15 @@ module.exports = {
   run: async (client, message, args) => {
     const user = message.mentions.members.first()
     if(!message.mentions.members.first()) {
-      message.channel.send("Please mention a user to strike.")
+      message.channel.send("Please mention a user to remove strikes from.")
     }
-    db.add(`strikes_${user}`, 1)
+    const amount = args[1]
+    db.subtract(`strikes_${user}`, amount)
     const strikesamount = db.get(`strikes_${user}`)
-    const strikeembed = new discord.MessageEmbed()
-   .setTitle("Striked!")
-   .setDescription(`Successfully striked <@${user.id}>, they are now at ${strikesamount} strikes!`)
+    const unstrikeembed = new discord.MessageEmbed()
+   .setTitle("Unstriked!")
+   .setDescription(`Successfully unstriked <@${user.id}>, they now have ${strikesamount} strikes left!`)
    .setTimestamp()
-   message.channel.send(strikeembed)
+   message.channel.send(unstrikeembed)
   }
 };
