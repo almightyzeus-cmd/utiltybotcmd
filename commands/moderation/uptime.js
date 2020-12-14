19fda282c813b8fd87364fc69835b3d9da61a620
Glitch (carnation-glib-trout) <none>
2020-12-14 17:04:47 +0000

    💁🗃 Checkpoint ./commands/moderation/uptime.js:23557778/420

--- commands/moderation/uptime.js
+++ commands/moderation/uptime.js
@@ -1,3 +1,4 @@
+const discord = require('discord.js')
 module.exports = {
 name: "uptime",
 usage: "wa!uptime",
@@ -13,12 +14,25 @@ run: async (client, message, args) => {
 let totalSeconds = (client.uptime / 1000);
 let days = Math.floor(totalSeconds / 86400);
 totalSeconds %= 86400;
+  
+  
 let hours = Math.floor(totalSeconds / 3600);
 totalSeconds %= 3600;
+  
+  
 let minutes = Math.floor(totalSeconds / 60);
+  
+  
 let seconds = Math.floor(totalSeconds % 60);
   
   
-  const
+  const uptimeembed = new discord.MessageEmbed()
+  .setTitle("Total Uptime:")
+  .addField("Days:", `${days}`)
+  .addField("Hours:", `${hours}`)
+  .addField("Minutes:", `${minutes}`)
+  .addField("Seconds:", `${seconds}`)
+  .setTimestamp()
+  message.channel.send(uptimeembed)
 }
 }
