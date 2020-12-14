ab6aa5261a4c70f14adfc1ab8eacf5f4d021efae
Glitch (carnation-glib-trout) <none>
2020-12-14 15:44:37 +0000

    💸💝 Checkpoint ./commands/moderation/purge.js:23557778/88

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -13,10 +13,7 @@ run: async (client, message, args) => {
   
   const deleteCount = parseInt(args[0], 10);
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 1 and 1000 for the number of messages to delete");
-    
-  
-  message.channel.send(`successfully purged ${deleteCount} messages!`)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
   
     const fetched = await message.channel.messages.fetch({limit: deleteCount});
     message.channel.bulkDelete(fetched)
